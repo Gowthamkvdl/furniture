ab1fdd38463e1583482271ba86fde70ccdbf4660
Gowtham K <gowthamkvdl@gmail.com>
2025-08-01 05:22:45 -0700

    hukyk

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -3,11 +3,13 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../navbar/Navbar";
 import Footer from "../footer/Footer";
 import { Toaster } from "react-hot-toast";
+import ScrollToTop from "../scrollToTop/ScrollToTop";
 
 export const Layout = () => {
   return (
     <div>
       <div className="container">
+        <ScrollToTop />
         <Toaster position="top-center" reverseOrder={false} />
         <div className="mt-md-5"></div>
         <Navbar />
